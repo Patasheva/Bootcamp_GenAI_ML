01f32f0920a42873f726f67b91e95343eb4baf32
PATASHEVA Olga <olgapatasheva@gmail.com>
2025-02-07 12:33:06 +0100

    start

---
+++ functions.py
@@ -0,0 +1,10 @@
+def check_weather():
+    weather = "sunny"
+    if weather == "sunny":
+       print("It's a sunny day! Enjoy!")
+    else:
+       return ("It might rain. Take an umbrella!")
+check_weather()
+
+
+
